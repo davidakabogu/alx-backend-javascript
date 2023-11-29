388c93f1ccff067cdecfaefc4d9ae14a037602f7
davidakabogu <akabogudavid@gmail.com>
2023-11-29 18:56:52 +0000

    include files

---
+++ 0x00-ES6_basic/100-createIteratorObject.js
@@ -0,0 +1,8 @@
+export default function createIteratorObject(report) {
+  const array = [];
+  for (const item of Object.values(report.allEmployees)) {
+    array.push(...item);
+  }
+
+  return array;
+}
