fe503e0ff426ba5ebd4b47fc96ab175912399012
DavidAkabogu <Akabogudavid@gmail.com>
2023-11-29 10:15:05 +0100

    format files with prettier

--- 0x00-ES6_basic/0-main.js
+++ 0x00-ES6_basic/0-main.js
@@ -1,5 +1,3 @@
-// eslint-disable-next-line import/extensions
-import { taskFirst, taskNext } from './0-constants.js';
+import { taskFirst, taskNext } from "./0-constants.js";
 
-// eslint-disable-next-line jest/require-hook
 console.log(`${taskFirst()} ${taskNext()}`);
