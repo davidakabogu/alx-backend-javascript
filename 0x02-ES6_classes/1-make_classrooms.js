f1866c76520cbfafa1595a1a16d2a3ed3ee10466
davidakabogu <akabogudavid@gmail.com>
2023-11-30 11:35:50 +0000

    eslint adjustments

--- 0x02-ES6_classes/1-make_classrooms.js
+++ 0x02-ES6_classes/1-make_classrooms.js
@@ -1,9 +1,9 @@
-import ClassRoom from './0-classroom'
+import ClassRoom from './0-classroom';
 
 export default function initializeRooms() {
-	const classRoomObject1 = new ClassRoom(19);
-	const classRoomObject2 = new ClassRoom(20);
-	const classRoomObject3 = new ClassRoom(34);
+  const classRoomObject1 = new ClassRoom(19);
+  const classRoomObject2 = new ClassRoom(20);
+  const classRoomObject3 = new ClassRoom(34);
 
-	return [classRoomObject1, classRoomObject2, classRoomObject3]
+  return [classRoomObject1, classRoomObject2, classRoomObject3];
 }
