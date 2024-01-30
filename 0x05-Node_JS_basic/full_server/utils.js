05f0131d4f9de24986573d200a92c4896da9c146
DavidAkabogu <Akabogudavid@gmail.com>
2024-01-30 06:19:10 +0100

    format code

--- 0x05-Node_JS_basic/full_server/utils.js
+++ 0x05-Node_JS_basic/full_server/utils.js
@@ -1,4 +1,4 @@
-const { readFile } = require('fs');
+const { readFile } = require("fs");
 
 module.exports = function readDatabase(filePath) {
   const students = {};
@@ -7,11 +7,11 @@ module.exports = function readDatabase(filePath) {
       if (err) {
         reject(err);
       } else {
-        const lines = data.toString().split('\n');
+        const lines = data.toString().split("\n");
         const noHeader = lines.slice(1);
         for (let i = 0; i < noHeader.length; i += 1) {
           if (noHeader[i]) {
-            const field = noHeader[i].toString().split(',');
+            const field = noHeader[i].toString().split(",");
             if (Object.prototype.hasOwnProperty.call(students, field[3])) {
               students[field[3]].push(field[0]);
             } else {
